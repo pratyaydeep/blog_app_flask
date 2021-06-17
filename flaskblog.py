f08104c2e2736f1e5f29bb1f4df349bb477d9c51
pratyaydeep <pratyaydeepghanta@gmail.com>
2021-06-17 17:24:02 +0530

    Worked around the template for login and register pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8baffc2cf8d4f5bd0fe18390c3154924'
 
 posts = [
     {
@@ -26,3 +29,24 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')  # success is the bootstrap class corresponding to success messege
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in !', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccesful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
