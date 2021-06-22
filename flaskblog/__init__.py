48ef737c472f1f409ee2bd26a59b4dd5f10fef22
pratyaydeep <pratyaydeepghanta@gmail.com>
2021-06-23 01:16:17 +0530

    Added functionalities for Create, Update and Delete posts

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,10 +6,11 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8baffc2cf8d4f5bd0fe18390c3154924'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Added by me not Corey
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'  # function name of the route login
+login_manager.login_view = 'login'  # function name of the route login (this is required for the login_required decorator to work)
 login_manager.login_message_category = 'info'
 
 from flaskblog import routes
